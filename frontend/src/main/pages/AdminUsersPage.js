7b0fe4a80c3befe58665cb4f42ea268660fa430a
Selena Zeng <selenazeng@ucsb.edu>
2025-05-22 14:00:05 -0700

    refresh the table data automatically after a successful toggle operation to give user immediate feedback

--- frontend/src/main/pages/AdminUsersPage.js
+++ frontend/src/main/pages/AdminUsersPage.js
@@ -1,12 +1,16 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UsersTable from "main/components/Users/UsersTable";
-
 import { useBackend } from "main/utils/useBackend";
+import { useCurrentUser } from "main/utils/currentUser";
+
 const AdminUsersPage = () => {
+  const currentUser = useCurrentUser();
+
   const {
     data: users,
     error: _error,
     status: _status,
+    refetch: refetchUsers
   } = useBackend(
     // Stryker disable next-line all : don't test internal caching of React Query
     ["/api/admin/users"],
@@ -17,7 +21,7 @@ const AdminUsersPage = () => {
   return (
     <BasicLayout>
       <h2>Users</h2>
-      <UsersTable users={users} />
+      <UsersTable users={users} currentUser={currentUser.data} onSuccess={refetchUsers} />
     </BasicLayout>
   );
 };
